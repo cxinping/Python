6647af1374f4797161e94f68017c33125e73f4d3
wangshuo <xpws2006@163.com>
2017-10-04 18:06:20 +0800

    逻辑回归算法

--- MachineLearning/回归分析算法/sk_LogisticRegression2.py
+++ MachineLearning/回归分析算法/sk_LogisticRegression2.py
@@ -46,7 +46,7 @@ def plot_decision_regions(X, y, classifier, test_idx=None, resolution=0.02):
         else:
             X_test, y_test = X[test_idx, :], y[test_idx]
         plt.scatter(X_test[:, 0], X_test[:, 1], c='', alpha=1.0, linewidth=1, marker='o',
-                    s=55, label='test set')
+                    s=55 )
 
 
 from sklearn.linear_model import LogisticRegression
