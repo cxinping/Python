c7cb772010b859718039e04d55493a5fd604fd89
wangshuo <xpws2006@163.com>
2017-10-04 16:30:15 +0800

    线性回归算法

--- MachineLearning/最邻近规则分类KNN算法/sk_knn.py
+++ MachineLearning/最邻近规则分类KNN算法/sk_knn.py
@@ -12,6 +12,7 @@ iris_y = iris.target
 ##print(iris_X[:2, :])
 ##print(iris_y)
 
+#把数据集分为训练集和测试集，其中 test_size=0.3，即测试集占总数据的 30%：
 X_train, X_test, y_train, y_test = train_test_split(
     iris_X, iris_y, test_size=0.3)
 
@@ -19,10 +20,6 @@ X_train, X_test, y_train, y_test = train_test_split(
 
 knn = KNeighborsClassifier()
 knn.fit(X_train, y_train)
+#对比用模型预测的值与真实的值，可以看到大概模拟出了数据，但是有误差，是不会完完全全预测正确的
 print(knn.predict(X_test))
 print(y_test)
-
-# 一般使用测试集打分
-print( knn.score( X_test, y_test ))
-
-
---
+++ MachineLearning/线性回归算法/sk_LinearRegression.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+
+"""
+最小二乘法线性回归函数
+
+"""
+from __future__ import print_function
+from sklearn import datasets
+from sklearn.linear_model import LinearRegression
+import matplotlib.pyplot as plt
+
+loaded_data = datasets.load_boston()
+data_X = loaded_data.data
+data_y = loaded_data.target
+
+model = LinearRegression()
+model.fit(data_X, data_y)
+
+print(model.predict(data_X[:4, :]))
+print(data_y[:4])
+
+X, y = datasets.make_regression(n_samples=100, n_features=1, n_targets=1, noise=1)
+plt.scatter(X, y)
+plt.show()
