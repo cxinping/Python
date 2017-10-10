ce12bd05dadbe410d8ef5f091a6a34d9dbcf7bd1
wangshuo <xpws2006@163.com>
2017-10-10 21:44:32 +0800

    Matplotlib 刻度

---
+++ matplotlib/ticks1.py
@@ -0,0 +1,33 @@
+# -*- coding: utf-8 -*-
+
+import matplotlib.pyplot as plt
+import numpy as np
+
+import matplotlib
+
+x = np.linspace( -3 , 3, 50  )
+y1 = 2 * x + 1
+y2 = x**2 + 1
+
+plt.figure(num=1,figsize=(8,5))
+plt.plot (x,y2 )
+plt.plot (x,y1,color='red', linewidth=1.0,linestyle='--')
+
+# 设置x轴刻度的取值范围
+plt.xlim((-1,2))
+#设置y轴刻度的取值范围
+plt.ylim((-2,3))
+
+plt.xlabel(u"I am  x")
+plt.ylabel(u"I am y ")
+
+# 设置x轴刻度的表现方式
+new_ticks = np.linspace(-1,2,5)
+plt.xticks(new_ticks)
+
+plt.yticks([-2,-1.8,-1,1.22,3],
+           [r'$really \ bad$',r'$bad$',r'$normal$',r'$good$',r'$really \ good$'] )
+
+plt.show()
+
+
