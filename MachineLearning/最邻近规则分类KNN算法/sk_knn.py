b0249b16069e63a9f37b0933ec73400fc2e94338
cxinping <xpws2006@163.com>
2017-10-29 19:19:08 +0800

    bayes算法

--- MachineLearning/最邻近规则分类KNN算法/sk_knn.py
+++ MachineLearning/最邻近规则分类KNN算法/sk_knn.py
@@ -1,6 +1,5 @@
 # -*- coding: utf-8 -*-
 
-from __future__ import print_function
 from sklearn import datasets
 from sklearn.model_selection import train_test_split
 from sklearn.neighbors import KNeighborsClassifier
