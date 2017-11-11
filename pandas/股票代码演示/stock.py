aa80e21a33b20003c4b37f2a9952d3969c7b94a0
cxinping <xpws2006@163.com>
2017-11-11 22:10:27 +0800

    股票代码演示

--- pandas/股票代码演示/stock.py
+++ pandas/股票代码演示/stock.py
@@ -5,7 +5,8 @@ import matplotlib.dates as mdates
 def datestr2num(s):
     return mdates.strpdate2num('%Y-%m-%d')(s)
 
-finance = pd.read_csv('amzn_stock.csv' , converters={0:datestr2num} )
+#finance = pd.read_csv('amzn_stock.csv' , converters={0:datestr2num} )
+finance = pd.read_csv('alibaba_stock.csv' , converters={0:datestr2num} )
 #finance = finance.head(10)
 plt.plot_date(finance['Date'],finance['Open'],'b-')
 plt.gcf().autofmt_xdate()  # 自动旋转日期标记
